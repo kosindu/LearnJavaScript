04e856f9fff6f093eaeb21ad6bfe9b1ee2bf9af6
kosindu nuwan <kbknbkasthuri@gmail.com>
2022-06-22 23:23:27 +0530

    To see that all things are in right order

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ console.log('Hello World');
 ! Javascript releases
 * ... -> ES5 -> |ES6/  | -> |ES7/  | -> |ES8/  | -> |ES9/  | -> |ES10/ | -> |ES11/ | -> ...
 *               |Es2015|    |ES2016|    |ES2017|    |ES2018|    |ES2019|    |ES2020|
-!               |__________________________________________________________________|
-*                                        Modern JavaScript
+!               |_______________________________.__________________________________|
+*                                         Modern JavaScript
 *
 ! ES6/2015 is the biggest update to the language EVER
 ? ES stands for ECMAScript
