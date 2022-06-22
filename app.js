ecb2569a4a06edb9ea7db071ce9823c9ffe949ac
kosindu nuwan <kbknbkasthuri@gmail.com>
2022-06-22 22:51:58 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('Hello World');
+
+/*
+! Javascript is a 
+* High-Level, (We don't have to worry about complex stuff like memory management) 
+* Object Oriented, (Based on objects, for storing most kind of data)
+* Multi-Paradigm, (We can use different styles of programming)
+! Programming Language.
+*/
+
+/*
+! Javascript releases
+* ... -> ES5 -> |ES6/  | -> |ES7/  | -> |ES8/  | -> |ES9/  | -> |ES10/ | -> |ES11/ | -> ...
+*               |Es2015|    |ES2016|    |ES2017|    |ES2018|    |ES2019|    |ES2020|
+!               |__________________________________________________________________|
+*                                        Modern JavaScript
+*
+! ES6/2015 is the biggest update to the language EVER
+? ES stands for ECMAScript
+*/
