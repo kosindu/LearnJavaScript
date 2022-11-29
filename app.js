8fea2d4efb450ce264ff6cfa203b47b4bb06302f
kosindu <kbknbkasthuri@gmail.com>
2022-11-30 00:04:57 +0530

    coding challenge 01

--- app.js
+++ app.js
@@ -485,7 +485,7 @@ TODO: we can use following Logical Assignment Operators for the above example
 ? man2.address ||= 'Colombo'
 
 * Here are the all Logical Assignment Operators
-? ||=, ??=, &&
+? ||=, ??=, &&=
 */
 
 /*
@@ -543,4 +543,86 @@ TODO: we can use following Logical Assignment Operators for the above example
 
 */
 
-console.log(0 ?? 'Kosindu'); //'Kosindu'
+// console.log(0 ?? 'Kosindu'); //'Kosindu'
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// const players1 = [...game.players[0]];
+// const players2 = [...game.players[1]];
+const [players1, players2] = game.players;
+
+// const team1_gk = players1[0];
+// const team1_fieldPlayers = players1.slice(1);
+// const team2_gk = players2[0];
+// const team2_fieldPlayers = players2.slice(1);
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// const team1 = game.odds.team1;
+// const draw = game.odds.x;
+// const team2 = game.odds.team2;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+// const printGoals = function (...players) {
+//   console.log(players);
+// 	const scored = game.scored;
+// 	for (let i = 0; i = scored.length; i++) {
+// 		for (let j = 0; j = players.length; j++) {
+// 			console.log()
+// 		}
+// };
+console.log(
+  players1,
+  players2,
+  gk,
+  fieldPlayers,
+  allPlayers,
+  players1Final,
+  team1,
+  draw,
+  team2
+);
+
+// printGoals('Davies', 'Muller', 'Lewasndowski', 'Kimrich');
